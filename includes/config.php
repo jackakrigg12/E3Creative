8b7689ec8af857e13ef75835aaf788ec1c87fc06
Jack Akrigg <38067995+jackakrigg12@users.noreply.github.com>
2018-04-20 14:55:14 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // DB Connections
-$config['DBHost'] = "46.32.240.33";
-$config['DBName'] = "jack--fyo-u-175877";
-$config['DBUser'] = "jack--fyo-u-175877";
-$config['DBPass'] = "D2Jghh6y.";
+$config['DBHost'] = "";
+$config['DBName'] = "";
+$config['DBUser'] = "";
+$config['DBPass'] = "";
 
 if (mysql_connect($config['DBHost'], $config['DBUser'], $config['DBPass'])) {
     $Connect = "TRUE";
